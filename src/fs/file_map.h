9c0de3bfa2893579732f4603cc747958584b8eaa
gunnerDgd <ifstudiomai@naver.com>
2024-05-14 01:34:37 +0900

    [Change] /fs/file_map.h added - Separated from /fs/file.h

--- src/fs/file_map.h
+++ src/fs/file_map.h
@@ -2,11 +2,11 @@
 #define __FS_FILE_H__
 
 #ifdef PRESET_WIN32
-#include "iocp/file.h"
+#include "iocp/file_map.h"
 #endif
 
 #ifdef PRESET_LINUX
-#include "epoll/file.h"
+#include "epoll/file_map.h"
 #endif
 
 #endif
