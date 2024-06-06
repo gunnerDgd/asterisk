260be599d613e3ff2c1acc4294b654596e5362d1
gunnerDgd <ifstudiomail@naver.com>
2024-06-07 04:29:33 +0900

    [Change] tcp_acpt -> acpt : Refactored For Generic Acceptor Socket (TCP / RUDP / QUIC / etc)

--- src/net/acpt.h
+++ src/net/acpt.h
@@ -1,10 +1,10 @@
-#ifndef __NET_TCP_ACPT_H__
-#define __NET_TCP_ACPT_H__
+#ifndef __NET_ACPT_H__
+#define __NET_ACPT_H__
 
 #ifdef PRESET_WIN32
 #include "iocp/tcp_acpt.h"
 #elif  PRESET_LINUX
-#include "epoll/tcp_acpt.h"
+#include "epoll/acpt.h"
 #endif
 
 #endif
