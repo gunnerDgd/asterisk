3ade1f8184cb45bc7a2738be0eb7d2d98970a92f
gunnerDgd <ifstudiomail@naver.com>
2024-06-07 04:25:29 +0900

    [Removal] end_v4 / end_v6 Removed - Use end_af instead

--- src/net/epoll/end.c
+++ src/net/epoll/end.c
@@ -15,31 +15,28 @@ obj_trait* end_t = &end_trait;
 
 bool_t
     end_new
-        (end* par_end, u32_t par_count, va_list par)                    {
-            obj*  addr = 0; if (par_count > 0) addr = va_arg(par, obj*) ;
-            u16_t port = 0; if (par_count > 1) port = va_arg(par, u32_t);
-            if (par_count == 0)                    {
-                par_end->af  = AF_UNSPEC           ;
-                par_end->len = sizeof(par_end->all);
+        (end* self, u32_t count, va_list arg)                       {
+            obj*  addr = 0; if (count > 0) addr = va_arg(arg, any_t);
+            u16_t port = 0; if (count > 1) port = va_arg(arg, u32_t);
+            if (count == 0)                                         {
+                self->len = sizeof(self->all);
+                self->af  = AF_UNSPEC        ;
                 return true_t ;
             }
-            if (trait_of(addr) == v4_t)                         {
-                par_end->v4.sin_family = AF_INET                ;
-                par_end->v4.sin_addr   = ((v4*)addr)->v4        ;
-                par_end->v4.sin_port   = be16(port)             ;
-                par_end->len           = sizeof(par_end->v4);
-                return true_t;
-            }
 
-            if (trait_of(addr) == v6_t)                      {
-                par_end->v6.sin6_family = AF_INET6           ;
-                par_end->v6.sin6_port   = be16(port)         ;
-                par_end->v6.sin6_addr   = ((v6*)addr)->v6    ;
-                par_end->len            = sizeof(par_end->v6);
-                return true_t;
-            }
-
+            if (trait_of(addr) == v4_t) goto v4;
+            if (trait_of(addr) == v6_t) goto v6;
             return false_t;
+    v4:     self->v4.sin_family = AF_INET         ;
+            self->v4.sin_addr   = ((v4*)addr)->v4 ;
+            self->v4.sin_port   = be16(port)      ;
+            self->len           = sizeof(self->v4);
+            return true_t;
+    v6:     self->v6.sin6_family = AF_INET6       ;
+            self->v6.sin6_addr   = ((v6*)addr)->v6;
+            self->v6.sin6_port   = be16(port)     ;
+            self->len           = sizeof(self->v4);
+            return true_t;
 }
 
 bool_t
@@ -55,41 +52,27 @@ void
         (end* par) {
 }
 
-bool_t
-    end_v4
-        (end* par)                                    {
-            if (trait_of(par) != end_t) return false_t;
-            return par->af == AF_INET;
-}
-
-bool_t
-    end_v6
-        (end* par)                                    {
-            if (trait_of(par) != end_t) return false_t;
-            return par->af == AF_INET6;
-}
-
 struct v4*
     end_as_v4
-        (end* par)                                   {
-            if (trait_of(par) != end_t) return null_t;
-            if (!end_v4 (par))          return null_t;
+        (end* self)                                   {
+            if (trait_of(self) != end_t) return null_t;
+            if (self->af != AF_INET)     return null_t;
             v4* ret = make (v4) from (0);
 
             if (trait_of(ret) != v4_t) return null_t;
-            ret->v4 = par->v4.sin_addr;
+            ret->v4 = self->v4.sin_addr;
             return ret;
 }
 
 struct v6*
     end_as_v6
-        (end* par)                                   {
-            if (trait_of(par) != end_t) return null_t;
-            if (!end_v6 (par))          return null_t;
+        (end* self)                                   {
+            if (trait_of(self) != end_t) return null_t;
+            if (self->af != AF_INET6)    return null_t;
             v6* ret = make (v6) from (0);
 
             if (trait_of(ret) != v6_t) return null_t;
-            ret->v6 = par->v6.sin6_addr;
+            ret->v6 = self->v6.sin6_addr;
             return ret;
 }
 
@@ -106,11 +89,11 @@ u16_t
 
 obj_trait*
     end_af
-        (end* par)                              {
-            if (trait_of(par) != end_t) return 0;
-            switch (par->af)                    {
-                case AF_INET : return v4_t;
-                case AF_INET6: return v6_t;
-                default      : return 0   ;
+        (end* par)                                   {
+            if (trait_of(par) != end_t) return null_t;
+            switch (par->af)                         {
+                case AF_INET : return   v4_t;
+                case AF_INET6: return   v6_t;
+                default      : return null_t;
             }
 }
--- src/net/epoll/end.h
+++ src/net/epoll/end.h
@@ -4,29 +4,28 @@
 #include <core.h>
 #include <arpa/inet.h>
 
+struct v4;
+struct v6;
+
 extern obj_trait *end_t     ;
 typedef struct    end       {
     obj                 head;
-    u64_t               len ;
-    union                   {
-    sa_family_t         af  ;
-    struct sockaddr_in  v4  ;
-    struct sockaddr_in6 v6  ;
-    struct sockaddr_in6 all ;
+    u64_t               len;
+    union                  {
+    struct sockaddr_in6 all;
+    struct sockaddr_in6 v6;
+    struct sockaddr_in  v4;
+    sa_family_t         af;
     };
 }   end;
 
-bool_t end_new  (end*, u32_t, va_list);
-bool_t end_clone(end*, end*)          ;
-void   end_del  (end*)                ;
+bool_t     end_new  (end*, u32_t, va_list);
+bool_t     end_clone(end*, end*)          ;
+void       end_del  (end*)                ;
 
-struct v4;
-struct v6;
 struct v4* end_as_v4(end*);
 struct v6* end_as_v6(end*);
 u16_t      end_port (end*);
-bool_t     end_v4   (end*);
-bool_t     end_v6   (end*);
 obj_trait* end_af   (end*);
 
 #endif
