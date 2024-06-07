495f2e494df3be629c8efeb31827eea6f0a83b74
gunnerDgd <ifstudiomail@naver.com>
2024-06-07 10:56:57 +0900

    [Example] use - dep Error : comma (,) must not be used in use () Statement

--- example/tcp/tcp.c
+++ example/tcp/tcp.c
@@ -5,16 +5,21 @@
 #include <stdio.h>
 
 use         (
-    dep(net), 
+    dep(net)
     dep(io)
 )
 
-run_async()                                                          {
-    end      *end_1  = make (end)      from (2, make_v4_int(0), 6500);
-    tcp_acpt *acpt_1 = make (tcp_acpt) from (1, end_1);
+run_async()                                                  {
+    end  *end_1  = make (end)  from (2, make_v4_int(0), 6500);
+    acpt *acpt_1 = make (acpt) from (0);
 
-    while(true_t)                            {
-        tcp *cli = await(tcp_acpt_run(acpt_1));
+    if (!acpt_open (acpt_1, end_1, tcp_t))   {
+        printf ("Faiiled to Open Acceptor\n");
+        return 1;
+    }
+
+    while(true_t)                         {
+        tcp *cli = await(acpt_fut(acpt_1));
         if (!cli)                                  {
             printf("Failed to Accept Connection\n");
             return 0;
--- example/udp/udp.c
+++ example/udp/udp.c
@@ -4,22 +4,26 @@
 
 #include <stdio.h>
 
-void run_async()                                                      {
-    io_sched *sched_1 = make (io_sched) from (0)                      ;
-    udp      *udp_1   = make (udp)      from (1, sched_1)             ;
-    end      *end_1   = make (end)      from (2, make_v4_int(0), 6500);
-    u8_t     *buf     = new  (u8_t[64]);
+use        (
+    dep (io)
+    dep (net)
+)
+
+run_async()                                                {
+    end  *end_1 = make (end) from (2, make_v4_int(0), 6500);
+    udp  *udp_1 = make (udp) from (0)                      ;
+    u8_t *buf   = new  (u8_t[64]);
     mem_set(buf, 0x00, 64);
     if (!udp_conn(udp_1, end_1)) {
         drop(buf)  ;
         del (end_1);
         del (udp_1);
 
-        return;
+        return 0;
     }
 
-    u64_t recv_size = await(udp_recv(udp_1, buf, 64));
-    printf("Received %s (%d Bytes)\n", buf, recv_size);
+    u64_t len = (u64_t) await(udp_recv(udp_1, buf, 64));
+    printf("Received %s (%d Bytes)\n", buf, len);
 
     drop(buf);
     del (udp_1);
