d99f16f65258f8fda0493213a04eae879517a237
gunnerDgd <ifstudiomail@naver.com>
2024-06-07 10:58:01 +0900

    [Change] udp_cli.c -> cli.c

---
+++ example/udp/cli.c
@@ -0,0 +1,23 @@
+#include <io.h>
+#include <net.h>
+#include <core.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+use         (
+    dep (io)
+    dep (net)
+)
+
+run_async()                                                          {
+    end *end_1 = make (end) from (2, make_v4_cstr("127.0.0.1"), 6500);
+    udp *udp_1 = make (udp) from (0)                                 ;
+
+    udp_open(udp_1, v4_t);
+    u64_t len = (u64_t) await(udp_send_to(udp_1, "Hello World\n", 12, end_1));
+    printf("Sent %d Bytes\n", len);
+    del(udp_1);
+    del(end_1);
+    return 0;
+}
