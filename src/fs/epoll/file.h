5a32a89159b502ae9abdce257e4bb1201b5c0983
gunnerDgd <ifstudiomail@naver.com>
2024-06-07 10:58:51 +0900

    [Feature] file_resize added for truncate operation

--- src/fs/epoll/file.h
+++ src/fs/epoll/file.h
@@ -29,8 +29,10 @@ void   file_close      (file*)                ;
 
 u64_t  file_seek       (file*, obj_trait*, u64_t);
 u64_t  file_pos        (file*, obj_trait*)       ;
-fut*   file_read       (file*, u8_t*, u64_t)  ;
-fut*   file_write      (file*, u8_t*, u64_t)  ;
-u64_t  file_size       (file*)                ;
+bool_t file_resize     (file*, u64_t)            ;
+
+fut*   file_write      (file*, u8_t*, u64_t);
+fut*   file_read       (file*, u8_t*, u64_t);
+u64_t  file_size       (file*)              ;
 
 #endif
