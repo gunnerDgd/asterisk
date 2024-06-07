5f473864cf3fa84ed0a8bfeb5312e64fc8044663
gunnerDgd <ifstudiomail@naver.com>
2024-06-07 10:58:41 +0900

    [Feature] file_resize added for truncate operation

--- include/fs/file.h
+++ include/fs/file.h
@@ -15,6 +15,8 @@ void   file_close      (file*)              ;
 
 u64_t  file_seek       (file*, obj_trait*, u64_t);
 u64_t  file_pos        (file*, obj_trait*)       ;
+bool_t file_resize     (file*, u64_t)            ;
+
 fut*   file_read       (file*, u8_t*, u64_t)  ;
 fut*   file_write      (file*, u8_t*, u64_t)  ;
 u64_t  file_size       (file*)                ;
