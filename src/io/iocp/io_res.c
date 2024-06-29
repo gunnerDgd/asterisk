bd9ae3fe05f8878e1fb855f2b9174d21cb177ec0
gunnerDgd <ifstudiomail@naver.com>
2024-06-29 23:05:56 +0900

    [Refactor] Static Function - Based Implementation

--- src/io/iocp/io_res.c
+++ src/io/iocp/io_res.c
@@ -1,69 +1,72 @@
 #include "io_res.h"
-#include "io_sched.h"
+#include "io_run.h"
 
+static u64_t
+    do_poll
+        (io_res* self)                                   {
+            if (trait_of(self) != io_res_t) return fut_err;
 
-obj_trait io_res_trait = make_trait (
-    io_res_new    ,
-    io_res_clone  ,
-    null_t        ,
-    io_res_del    ,
-    sizeof(io_res),
-    null_t
-);
-
-obj_trait* io_res_t = &io_res_trait;
-
-u64_t
-    io_res_do_poll
-        (io_res* par)                                    {
-            if (trait_of(par) != io_res_t) return fut_err;
-            if (par->stat == fut_pend)     io_sched_run(par->sched);
-            return par->stat;
+            if (self->stat == fut_pend) io_run_flush(self->run);
+            return self->stat;
 }
 
-void*
-    io_res_do_ret
-        (io_res* par)                                   {
-            if (trait_of(par) != io_res_t) return null_t;
-            return par->ret;
+static any_t
+    do_ret
+        (io_res* self)                                   {
+            if (trait_of(self) != io_res_t) return null_t;
+            return self->ret;
 }
 
-fut_ops io_res_fut_ops = make_fut_ops (
-    io_res_do_poll,
-    io_res_do_ret
+static fut_ops 
+    do_fut = make_fut_ops (
+        do_poll,
+        do_ret
 );
 
-bool_t 
-    io_res_new
-        (io_res* par_res, u32_t par_count, va_list par)                           {
-            io_sched* sched = 0; if (par_count > 0) sched = va_arg(par, io_sched*);
-            if (trait_of(sched) != io_sched_t) return false_t;
-            mem_set(&par_res->res, 0x00, sizeof(OVERLAPPED));
-            par_res->sched = ref(sched);
-            par_res->stat  = fut_pend  ;
-            par_res->ret   = 0         ;
+static bool_t 
+    do_new
+        (io_res* self, u32_t count, va_list arg)                         {
+            io_run *run = null_t; if (count > 0) run = va_arg(arg, any_t);
+            if (trait_of(run) != io_run_t) return false_t;
+
+            mem_set(&self->res, 0x00, sizeof(OVERLAPPED));
+            self->stat = fut_pend;
+            self->run  = ref(run);
+            self->ret  = 0       ;
             return true_t;
 }
 
-bool_t 
-    io_res_clone
-        (io_res* par, io_res* par_clone) {
+static bool_t 
+    do_clone
+        (io_res* self, io_res* clone) {
             return false_t; 
 }
 
-void
-    io_res_del  
-        (io_res* par)       {
-            del (par->sched);
+static void
+    do_del  
+        (io_res* self)     {
+            del (self->run);
 }
+
+static obj_trait 
+    do_obj = make_trait (
+        do_new        ,
+        do_clone      ,
+        null_t        ,
+        do_del        ,
+        sizeof(io_res),
+        null_t
+);
+
+obj_trait* io_res_t = &do_obj;
 
 fut*
     io_res_fut
-        (io_res* par)                              {
-            if (trait_of(par) != io_res_t) return 0;
-            return make (fut) from                 (
-                2              ,
-                &io_res_fut_ops,
-                par
+        (io_res* self)                                   {
+            if (trait_of(self) != io_res_t) return null_t;
+            return make (fut) from        (
+                2      ,
+                &do_fut,
+                self
             );
 }
--- src/io/iocp/io_res.h
+++ src/io/iocp/io_res.h
@@ -1,22 +1,19 @@
-#ifndef __IOCP_IO_RES_H__
-#define __IOCP_IO_RES_H__
+#ifndef IO_IOCP_IO_RES_H
+#define IO_IOCP_IO_RES_H
 
 #include <core.h>
-#include "io_sched.h"
+#include "io_run.h"
 
 extern obj_trait* io_res_t;
 typedef struct    io_res  {
-	obj		   head ;
-	io_sched  *sched;
-	u64_t	   stat ;
-	OVERLAPPED res  ;
-	void*	   ret  ;
-	void*	   arg  ;
+	obj		   head;
+	u64_t	   stat;
+	io_run    *run;
+	OVERLAPPED res;
+	void*	   ret;
+	void*	   arg;
 }	io_res;
 
-bool_t io_res_new  (io_res*, u32_t, va_list);
-bool_t io_res_clone(io_res*, io_res*)       ;
-void   io_res_del  (io_res*)				;
-fut*   io_res_fut  (io_res*)				;
+fut* io_res_fut(io_res*);
 
 #endif
