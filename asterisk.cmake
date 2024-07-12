4c181261af5834a1b5e1d3f877de3601972b6edf
gunnerDgd <ifstudiomail@naver.com>
2024-07-12 19:22:41 +0900

    [Change] Now preset checking part is moved under asterisk.cmake

---
+++ asterisk.cmake
@@ -0,0 +1,33 @@
+if   (NOT PRESET_FEATURE_THREAD)
+    message("[asterisk] asterisk requires \"thread\" feature of crisp.")
+    message(SEND_ERROR)
+endif()
+
+if   (NOT DEFINED PRESET_ARCH)
+    message ("[asterisk] Architecture (PRESET_ARCH_XXX) Not Defined")
+    message (SEND_ERROR)
+endif()
+
+if   (NOT DEFINED PRESET_OS)
+    message ("[asterisk] Base OS (PRESET_LINUX / PRESET_WIN32 / etc) Not Defined")
+    message (SEND_ERROR)
+endif()
+
+if   (NOT DEFINED PRESET_COMPILER)
+    message ("[asterisk] Compiler (PRESET_COMPILER_XXX) Not Defined")
+    message (SEND_ERROR)
+endif()
+
+if   (NOT CRISP_TLS)
+    message("[asterisk] asterisk requires CRISP_TLS for thread safety.")
+    message(SEND_ERROR)
+endif()
+
+if   (NOT CRISP)
+    message("[asterisk] asterisk requires crisp")
+    message(SEND_ERROR)
+endif()
+
+macro   (asterisk_examples)
+    add_subdirectory(lib/asterisk/example)
+endmacro()
